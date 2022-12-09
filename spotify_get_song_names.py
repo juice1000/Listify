4a813cc52d8549361600252c8909bb0f48e74b9a
juice1000 <julienlook@gmx.de>
2022-12-09 12:49:49 +0100

    deprecate spotify api utilizing script, create new one based on web scraping method

--- spotify_get_song_names.py
+++ spotify_get_song_names.py
@@ -1,45 +1,70 @@
-import spotipy
-from spotipy import util
-import subprocess
+import requests
+from bs4 import BeautifulSoup
 import json
-
-# Search for name
-# TODO: needs to be refactored if we want to make an app out of this
-scope = 'user-library-read'
-username = 'Julien Look'
-
-
-def authorization():
-    token = util.prompt_for_user_token(username,
-    scope,
-    client_id='54372e1b817d4af19982352a52541a48',
-    client_secret='ef3cdce4889e4a55aff19bebbdaa5c24',
-    redirect_uri='http://localhost:9090')
-    auth = f"Authorization: Bearer {token}"
-    return auth
+from selenium.webdriver import Chrome
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.common.by import By
+from time import sleep
+import re
 
 
-def retrieve_playlist_songs(playlist_id, debug):
-    auth = authorization()
 
-    song_limit = ''
-    if debug:
-        song_limit = '&limit=1'
-        
-    json_file = subprocess.check_output(["curl", "-X", "GET", f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks?fields=items(track(name%2C%20album(artists(name)))){song_limit}", "-H", auth, '--silent'])
+def extract_playlist_meta_information(driver):
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    track_metadata = soup.find_all('meta', {'name': 'music:song'})
 
-    song_titles = []
-
-    if str(json_file) != "b''" and str(json_file).find("Error") == -1 and str(json_file).find("error") == -1:
-
-        dictionary = json.loads(json_file)
-        for item in dictionary.values():
-            for value in item:
-                track_name = value.get('track', {}).get('name', {})
-                artists = value.get('track', {}).get('album', {}).get('artists', {})
-                artist_name = artists[0].get('name', {})
-
-                song_titles.append(f"{track_name}  by  {artist_name}")
-
+    song_urls = []
+    for element in track_metadata:
+        element_url = element.attrs['content']
+        song_urls.append((element_url))
     
-    return song_titles
+    return song_urls
+
+
+def extract_data_from_song_url(driver):
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+    # TODO: #1 this is quite unstable the way we do it, let's find a better solution!
+    track_elements = soup.find_all('title')[0].contents[0].split(('- song and lyrics by'))
+    song_name = track_elements[0].strip()
+    artist_name = re.sub( '\|', '',re.sub( 'Spotify', '', track_elements[1])).strip()
+    return (song_name, artist_name)
+
+
+# using selenium and beautiful soup
+def track_data_extractor(URL):
+    options = ChromeOptions()
+    options.headless = True
+
+    # TODO: needs to be stored on webserver too when we're ready to deploy
+    driver = Chrome(executable_path='/Users/julienlook/Documents/Coding/spotify_downloader/chromedriver', options=options)
+    driver.get(url=URL)
+
+    # extract information from html head
+    # TODO: do this for all titles and put in array
+    extracted_song_url = extract_playlist_meta_information(driver)
+    driver.quit()
+    tracks = []
+    for song_url in extracted_song_url:
+        # relaunch driver
+        driver = Chrome(executable_path='/Users/julienlook/Documents/Coding/spotify_downloader/chromedriver', options=options)
+        driver.get(url=song_url)
+
+        track_info = extract_data_from_song_url(driver)
+
+        # TODO: store this in another array (check array format we prepped for the youtube download)
+        print(track_info)
+        tracks.append(track_info)
+        
+        driver.quit()
+    
+    return tracks
+
+
+
+# TODO: we need a function that checks if the input source is truly a playlist of just a song
+# TODO: we could create a 2 stream product that can download songs or playlists
+
+# TODO this needs to be left to the app input prompt
+URL = "https://open.spotify.com/playlist/1QzMPmOyuxetr3Mbw4vBb8"
+track_data_extractor(URL)
