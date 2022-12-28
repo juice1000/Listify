a38e2cb6d8489901d90ef9a22f54ee2f2845c1df
juice1000 <julienlook@gmx.de>
2022-12-28 17:37:34 +0100

    get functioning progress bar in flask app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash, send_file, after_this_request
+from flask import Flask, render_template, request, flash, send_file, Response
 import spotify_get_song_names as spt
 import youtube_downloader as yt
 from config import Prod, Dev
@@ -7,6 +7,7 @@ import os
 from os.path import basename
 from io import BytesIO
 import shutil
+import time
 
 import os
 app = Flask(__name__)
@@ -47,6 +48,20 @@ def download():
     data.seek(0)
     shutil.rmtree(path, ignore_errors=False, onerror=None)
     return send_file(data, mimetype='application/zip', as_attachment=True, download_name='music_playlist.zip')
+
+
+
+@app.route('/progress')
+def progress():
+	def generate():
+		x = 0
+		
+		while x <= 100:
+			yield "data:" + str(x) + "\n\n"
+			x = x + 10
+			time.sleep(0.5)
+
+	return Response(generate(), mimetype= 'text/event-stream')
 
 
 if __name__ == "__main__":
